21880e8a4fc611fb0544747dc14c9e9915844280
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-02-09 14:41:34 -0500

    Added functionality to user Sign Up fields, db integration, and flash errors/success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
-    @title = User.name
+    @user  = User.find(params[:id])
+    @title = User.name    
   end
 
-  def new
-    @title = "Sign Up"
+  def new  
     @user  = User.new
+    @title = "Sign Up"
+  end
+
+  def create
+     
+    @user  = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :flash => { :success  => "Welcome to the Sample App!"}
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
