858e9383077545a7fb8fd614846756e364e11ff5
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-02-08 19:44:09 -0500

    Added a sidebar and some styling

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ SampleApp::Application.routes.draw do
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
-	get "pages/home"
+	
 
 
   # The priority is based upon order of creation:
