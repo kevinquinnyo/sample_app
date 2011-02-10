b9257dad2496943289d7f855c7fa101882eeb937
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-02-09 20:45:16 -0500

    Working User Sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110208220126
+# Schema version: 20110208223655
 #
 # Table name: users
 #
@@ -9,6 +9,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -34,10 +35,17 @@ class User < ActiveRecord::Base
   end
 
   class << self
-    def User.authenticate(email, submitted_password)
+    def authenticate(email, submitted_password)
       user = find_by_email(email)
-      return nil  if user.nil?
-      return user if user.has_password?(submitted_password)    
+      (user && user.has_password?(submitted_password)) ? user : nil
+      # above one-liner is the same as:     
+      #return nil  if user.nil?
+      #return user if user.has_password?(submitted_password)    
+    end
+
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
     end
 
   end
